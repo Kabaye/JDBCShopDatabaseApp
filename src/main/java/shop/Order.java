bca5ed09568ba5945fb344ad4cc5ddd85d3e99d1
Kabaye <svkulich@gmail.com>
2019-04-02 23:33:22 +0300

    style: change name of field to more sensible

--- src/main/java/shop/Order.java
+++ src/main/java/shop/Order.java
@@ -4,12 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor(staticName = "of")
 public class Order {
     private int orderId;
     private int customerId;
-    private int goodId;
-    private int amount;
+    private List<Integer> goodsId;
+    private List<Integer> amounts;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", customerId=" + customerId +
+                ", goodsId=" + goodsId +
+                ", amounts=" + amounts +
+                '}';
+    }
+
+    public void addGood(int goodID, int amount)
+    {
+        goodsId.add(goodID);
+        amounts.add(amount);
+    }
+
 }
