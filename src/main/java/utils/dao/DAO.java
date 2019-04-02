2c6a429bd74327e1b6b4943289537354a1f47297
Kabaye <svkulich@gmail.com>
2019-04-03 00:41:51 +0300

    feat: add DAO interface

--- src/main/java/utils/dao/DAO.java
+++ src/main/java/utils/dao/DAO.java
@@ -3,9 +3,11 @@ package utils.dao;
 import java.util.List;
 
 public interface DAO<T> {
+
     T create (T t);
     T read(int id);
     List<T> findAll();
+
     T update(T t);
     void delete(int id);
 }
