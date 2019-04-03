485eea8b1ab51d22d57238d455a789ed2ad0f948
Kabaye <svkulich@gmail.com>
2019-04-03 18:37:18 +0300

    feat: change realization of update method, add some new features

--- src/main/java/utils/dao/order/OrderDaoImpl.java
+++ src/main/java/utils/dao/order/OrderDaoImpl.java
@@ -1,15 +1,11 @@
 package utils.dao.order;
 
-import lombok.NonNull;
 import shop.Order;
 import utils.connection.ConnectionBuilder;
 import utils.connection.SimpleConnectionBuilder;
 import utils.dao.DAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -20,13 +16,18 @@ public class OrderDaoImpl implements DAO<Order> {
     private static final String EXCEPTION_MESSAGE = "404 Error! No connection to database!";
 
     private static final String INSERT
-            = "INSERT INTO orders (order_id, customer_id, good_id, amount) VALUES (?, ?, ?, ?)";
+            = "INSERT INTO orders (order_id, customer_id, good_id, amount,id) VALUES (?, ?, ?, ?,?)";
 
     private static final String SELECT_ORDERS_BY_ID
-            = "SELECT order_id, customer_id, good_id, amount FROM orders WHERE order_id=?";
+            = "SELECT order_id, customer_id, good_id, amount, id FROM orders WHERE order_id=?";
 
     private static final String SELECT_ALL
             = "SELECT * FROM orders";
+
+    private static final String UPDATE
+            = "UPDATE orders SET good_id=?, amount=? where id=? and order_id=? and customer_id=?";
+
+    private static final String GET_MAX_ID = "SELECT MAX(id) as MAX_ID FROM orders";
 
     private static final String DELETE
             = "DELETE FROM orders WHERE order_id=?";
@@ -34,17 +35,28 @@ public class OrderDaoImpl implements DAO<Order> {
 
     private ConnectionBuilder builder = new SimpleConnectionBuilder();
 
-    //add order with unique customer
+    //add order
     public Order create(Order order) {
         try {
             Connection con = builder.getConnection();
-            PreparedStatement pst = con.prepareStatement(INSERT);
-            for (int i = 0; i < order.getAmounts().size(); i++) {
-                pst.setInt(1, order.getOrderID());
-                pst.setInt(2, order.getCustomerID());
-                pst.setInt(3, order.getGoodIDs().get(i));
-                pst.setInt(4, order.getAmounts().get(i));
-                pst.executeUpdate();
+
+            PreparedStatement pst = con.prepareStatement(GET_MAX_ID);
+            ResultSet set = pst.executeQuery();
+            if (set.next()) {
+                int counter = set.getInt("MAX_ID") + 1;
+                pst.close();
+                set.close();
+                pst = con.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+                for (int i = 0; i < order.getAmounts().size(); i++) {
+                    pst.setInt(1, order.getOrderID());
+                    pst.setInt(2, order.getCustomerID());
+                    pst.setInt(3, order.getGoodIDs().get(i));
+                    pst.setInt(4, order.getAmounts().get(i));
+                    pst.setInt(5, counter++);
+                    pst.executeUpdate();
+                    pst.close();
+                }
+                con.close();
             }
         } catch (
                 SQLException exc) {
@@ -54,33 +66,37 @@ public class OrderDaoImpl implements DAO<Order> {
     }
 
     //get order
-    public Order read(long id) {
+    public Order read(long orderID) {
         Order order = null;
         try {
             Connection con = builder.getConnection();
             PreparedStatement pst = con.prepareStatement(SELECT_ORDERS_BY_ID);
-            pst.setLong(1, id);
+            pst.setLong(1, orderID);
             ResultSet orderSet = pst.executeQuery();
 
-            int orderID;
+            int orderID1;
             int customerID;
+            List<Integer> IDs = new ArrayList<>();
             List<Integer> goods = new ArrayList<>();
             List<Integer> amount = new ArrayList<>();
 
             if (orderSet.next()) {
-                orderID = orderSet.getInt("order_id");
+                orderID1 = orderSet.getInt("order_id");
                 customerID = orderSet.getInt("customer_id");
+                IDs.add(orderSet.getInt("id"));
                 goods.add(orderSet.getInt("good_id"));
                 amount.add(orderSet.getInt("amount"));
                 while (orderSet.next()) {
+                    IDs.add(orderSet.getInt("id"));
                     goods.add(orderSet.getInt("good_id"));
                     amount.add(orderSet.getInt("amount"));
                 }
-                order = Order.of(orderID, customerID, goods, amount);
+                order = Order.of(orderID1, customerID, IDs, goods, amount);
             }
 
             orderSet.close();
             pst.close();
+            con.close();
         } catch (SQLException exc) {
             System.out.println(EXCEPTION_MESSAGE);
         }
@@ -101,37 +117,56 @@ public class OrderDaoImpl implements DAO<Order> {
     }
 
     public Order update(Order order) {
-        this.delete(order.getOrderID());
-        this.create(order);
+        try {
+            Connection con = builder.getConnection();
+            PreparedStatement pst = con.prepareStatement(UPDATE);
+            for (int i = 0; i < order.getGoodIDs().size(); i++) {
+                pst.setInt(1, order.getGoodIDs().get(i));
+                pst.setInt(2, order.getAmounts().get(i));
+                pst.setInt(3, order.getIDs().get(i));
+                pst.setInt(4, order.getOrderID());
+                pst.setInt(5, order.getCustomerID());
+                pst.executeUpdate();
+            }
+            pst.close();
+            con.close();
+        } catch (SQLException exc) {
+            System.out.println(EXCEPTION_MESSAGE);
+        }
         return order;
     }
 
     //удаляю весь заказ((
-    public void delete(long id) {
+    public void delete(long orderID) {
         try {
             Connection con = builder.getConnection();
             PreparedStatement pst = con.prepareStatement(DELETE);
-            pst.setLong(1, id);
+            pst.setLong(1, orderID);
             pst.executeUpdate();
+            pst.close();
+            con.close();
         } catch (SQLException exc) {
             System.out.println(EXCEPTION_MESSAGE);
         }
     }
 
-    private void addAll(@NonNull ResultSet set, List<Order> orders) throws SQLException {
+    private void addAll(ResultSet set, List<Order> orders) throws SQLException {
         Set<Integer> orderIDs = new TreeSet<>();
         int customerID;
         List<Integer> goods;
         List<Integer> amounts;
+        List<Integer> IDs;
         while (set.next()) {
             final int orderID = set.getInt("order_id");
             if (orderIDs.add(orderID)) {
                 customerID = set.getInt("customer_id");
+                IDs = new ArrayList<>();
+                IDs.add(set.getInt("id"));
                 goods = new ArrayList<>();
                 goods.add(set.getInt("good_id"));
                 amounts = new ArrayList<>();
                 amounts.add(set.getInt("amount"));
-                orders.add(Order.of(orderID, customerID, goods, amounts));
+                orders.add(Order.of(orderID, customerID, IDs, goods, amounts));
             } else {
                 Order order = orders.stream()
                         .filter(elem -> elem.getOrderID() == orderID).findFirst().get();
